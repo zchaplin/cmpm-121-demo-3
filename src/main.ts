ecf60eea5f840622952af962cf82aa69960676c5
Adam Smith <adam@adamsmith.as>
2023-10-30 16:28:40 -0700

    Prepare starter code for D3

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,76 @@
+import "leaflet/dist/leaflet.css";
 import "./style.css";
+import leaflet from "leaflet";
+import luck from "./luck";
 
-const app: HTMLDivElement = document.querySelector("#app")!;
+const MERRILL_CLASSROOM = leaflet.latLng({
+    lat: 36.9995,
+    lng: - 122.0533
+});
 
-const gameName = "My game";
+const GAMEPLAY_ZOOM_LEVEL = 19;
+const TILE_DEGREES = 1e-4;
+const NEIGHBORHOOD_SIZE = 8;
+const PIT_SPAWN_PROBABILITY = 0.1;
 
-document.title = gameName;
+const mapContainer = document.querySelector<HTMLElement>("#map")!;
 
-const header = document.createElement("h1");
-header.innerHTML = gameName;
-app.append(header);
+const map = leaflet.map(mapContainer, {
+    center: MERRILL_CLASSROOM,
+    zoom: GAMEPLAY_ZOOM_LEVEL,
+    minZoom: GAMEPLAY_ZOOM_LEVEL,
+    maxZoom: GAMEPLAY_ZOOM_LEVEL,
+    zoomControl: false,
+    scrollWheelZoom: false
+});
+
+leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>"
+}).addTo(map);
+
+const playerMarker = leaflet.marker(MERRILL_CLASSROOM);
+playerMarker.bindTooltip("That's you!");
+playerMarker.addTo(map);
+
+let points = 0;
+const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
+statusPanel.innerHTML = "No points yet...";
+
+function makePit(i: number, j: number) {
+    const bounds = leaflet.latLngBounds([
+        [MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
+        MERRILL_CLASSROOM.lng + j * TILE_DEGREES],
+        [MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
+        MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES],
+    ]);
+
+    const pit = leaflet.rectangle(bounds) as leaflet.Layer;
+
+
+
+    pit.bindPopup(() => {
+        let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+        const container = document.createElement("div");
+        container.innerHTML = `
+                <div>There is a pit here at "${i},${j}". It has value <span id="value">${value}</span>.</div>
+                <button id="poke">poke</button>`;
+        const poke = container.querySelector<HTMLButtonElement>("#poke")!;
+        poke.addEventListener("click", () => {
+            value--;
+            container.querySelector<HTMLSpanElement>("#value")!.innerHTML = value.toString();
+            points++;
+            statusPanel.innerHTML = `${points} points accumulated`;
+        });
+        return container;
+    });
+    pit.addTo(map);
+}
+
+for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
+    for (let j = - NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+        if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
+            makePit(i, j);
+        }
+    }
+}
